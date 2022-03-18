82b5ff4515d465dfaf6713756447cc618f6d4a72
JuanE <juanernestogranados@gmail.com>
2022-03-18 08:13:50 -0500

    Funcion sumame y codigos funcionales añadidos

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
-//Hola Maker,
+// console.log(`hola mundo`);
 
-// Tu misión en este reto es escribir una función llamada adjustText que ajuste un string a una longitud determinada.
+// function factorial(n = 1) {
+//   let nFactorial = 1;
+//   for (let i = 0; i < n - 1; i++) {
+//     nFactorial = (n - i) * nFactorial;
+//   }
+//   return nFactorial;
+// }
 
-// La función debe recibir dos argumentos: un string y un número, que determina la longitud a la que se debe ajustar el string.
-// Si la longitud del string es mayor al valor del segundo parámetro, se debe cortar. De lo contrario, se debe completar con espacios.
-// La función debe retornar el string ajustado.
-const b = "hola";
-//console.log(b.charAt(4) === "");
+// console.log(factorial(5));
+//
 
-function adjustText(s, n) {
-  if (s.charAt(n) === "") {
-    for (let i = s.length; i < n; i++) {
-      s = s.concat(" ");
-    }
-    return s;
-  } else {
-    return s.slice(0, -Math.abs(n - s.length));
-  }
-}
+//----------------------Sumame ambas opciones sirven
 
-// Ejemplos:
-// adjustText("", 3); //=> "   "
-// adjustText("hola", 2); //=> "ho"
-// adjustText("Hola", 0); //=> ""
-// adjustText("Hola", 10); //=> "Hola      "
-console.log(` '${adjustText("", 3)}'
-  '${adjustText("hola", 2)}'
-  '${adjustText("Hola", 0)}'
-  '${adjustText("Hola", 10)}'`);
-
-function adjustText(str, length) {
-  if (str.length > length) {
-    return str.substring(0, length);
-  } else {
-    return str + " ".repeat(length - str.length);
-  }
-}
-function adjustText(str, n) {
-  return str.length > n ? str.slice(0, n) : str.padEnd(n);
-}
-const adjustText = (text, number) => {
-  let transformed = "";
-  for (let i = 0; i < number; i++) {
-    if (text.charAt(i) === "") transformed += "5";
-    else transformed += text.charAt(i);
-  }
-  return transformed;
+const sumame = function (arr) {
+  return arr.reduce((partialSum, a) => partialSum + a, 0);
 };
+//const sumame = (arr) => arr.reduce((a, b) => a + b, 0);
+
+console.log(`${sumame([1, 1, 2, 10])}`);
+
+// function reverseWords(str) {
+//   let s = str.split(" ");
+//   let sPrime = [];
+//   console.log(s);
+//   for (let i = 0; i < s.length; i++) {
+//     sPrime.push(s[s.length - 1 - i]);
+//   }
+//   s = sPrime.join(" ");
+//   return s; // reverse those words
+// }
+
+// console.log(`${reverseWords("soy un string de varias palabras")}`);
+
+// [Boolean, Number, Array].forEach(
+//   (obj) =>
+//     (obj.prototype.toString1 = function () {
+//       return JSON.stringify(this);
+//     })
+// );
+
+// console.log(
+//   true.toString1(),
+//   false.toString1(),
+//   [].toString1() === "[]",
+//   [].toString1(),
+//   [1].toString1() === "[1]",
+//   [1].toString1(),
+//   [2, 4, 8, 17].toString1() === "[2,4,8,17]",
+//   [2, 4, 8, 17].toString1(),
+//   [Math.PI, Math.E].toString1() === "[3.141592653589793,2.718281828459045]",
+//   [Math.PI, Math.E].toString1()
+// );
