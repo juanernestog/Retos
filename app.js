7b2a1eee5349785469d01c838c219af1b44da66d
JuanE <juanernestogranados@gmail.com>
2022-03-18 08:55:54 -0500

    Funciones sin comentarios

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-// console.log(`hola mundo`);
+function factorial(n = 1) {
+  let nFactorial = 1;
+  for (let i = 0; i < n - 1; i++) {
+    nFactorial = (n - i) * nFactorial;
+  }
+  return nFactorial;
+}
 
-// function factorial(n = 1) {
-//   let nFactorial = 1;
-//   for (let i = 0; i < n - 1; i++) {
-//     nFactorial = (n - i) * nFactorial;
-//   }
-//   return nFactorial;
-// }
-
-// console.log(factorial(5));
-//
+console.log(factorial(5));
 
 //----------------------Sumame ambas opciones sirven
 
-// const sumame = function (arr) {
-//   return arr.reduce((partialSum, a) => partialSum + a, 0);
-// };
-//const sumame = (arr) => arr.reduce((a, b) => a + b, 0);
+const sumame = function (arr) {
+  return arr.reduce((partialSum, a) => partialSum + a, 0);
+};
+const sumame2 = (arr) => arr.reduce((a, b) => a + b, 0);
 
 //console.log(`${sumame([1, 1, 2, 10])}`);
 
@@ -26,27 +23,27 @@ const promediame = function (arr) {
   }
   return arr.reduce((partialSum, a) => partialSum + a, 0) / arr.length;
 };
-console.log(`Solucion ${promediame([])} ${promediame([1, 2, 3, 4])}`);
+//console.log(`Solucion ${promediame([])} ${promediame([1, 2, 3, 4])}`);
 
-// function reverseWords(str) {
-//   let s = str.split(" ");
-//   let sPrime = [];
-//   console.log(s);
-//   for (let i = 0; i < s.length; i++) {
-//     sPrime.push(s[s.length - 1 - i]);
-//   }
-//   s = sPrime.join(" ");
-//   return s; // reverse those words
-// }
+function reverseWords(str) {
+  let s = str.split(" ");
+  let sPrime = [];
+  console.log(s);
+  for (let i = 0; i < s.length; i++) {
+    sPrime.push(s[s.length - 1 - i]);
+  }
+  s = sPrime.join(" ");
+  return s; // reverse those words
+}
 
 // console.log(`${reverseWords("soy un string de varias palabras")}`);
 
-// [Boolean, Number, Array].forEach(
-//   (obj) =>
-//     (obj.prototype.toString1 = function () {
-//       return JSON.stringify(this);
-//     })
-// );
+[Boolean, Number, Array].forEach(
+  (obj) =>
+    (obj.prototype.toString1 = function () {
+      return JSON.stringify(this);
+    })
+);
 
 // console.log(
 //   true.toString1(),
